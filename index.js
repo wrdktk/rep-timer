be8fb2578f273229f9b568ea63a4bb1547ed5215
warda <warda@care3.co>
2020-02-14 20:31:18 +0000

    programmed stroke offset to reduce by 1px per countdown tick

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+const stroke = circle.setAttribute('stroke-dasharray', perimeter);
 
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log('timer started');
   },
   onTick() {
-    console.log('timer ticked');
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset -= 1;
   },
   onComplete() {
     console.log('timer completed');
