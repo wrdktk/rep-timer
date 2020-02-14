beee70d4871595d8ba48008f3dc13de9470156c7
warda <warda@care3.co>
2020-02-14 19:54:23 +0000

    moved timer instance back to index.js

--- timer.js
+++ timer.js
@@ -46,19 +46,3 @@ class Timer {
     this.durationInput.value = time;
   }
 }
-
-const durationInput = document.querySelector('#duration');
-const startButton = document.querySelector('#start');
-const pauseButton = document.querySelector('#pause');
-
-const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart() {
-    console.log('timer started');
-  },
-  onTick() {
-    console.log('timer ticked');
-  },
-  onComplete() {
-    console.log('timer completed');
-  },
-});
